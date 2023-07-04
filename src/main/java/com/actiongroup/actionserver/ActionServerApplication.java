17c541690242574405e2de657257b2101d0ef8b9
EDmitryV <edv.busines@gmail.com>
2023-07-04 12:40:27 +0500

    Init commit

---
+++ src/main/java/com/actiongroup/actionserver/ActionServerApplication.java
@@ -0,0 +1,13 @@
+package com.actiongroup.actionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actiongroup/actionserver/ActionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actiongroup.actionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
