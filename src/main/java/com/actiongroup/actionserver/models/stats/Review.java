da746e48ec12a709def2913a5dd5698a3a08ecaa
stlad <82213506+stlad@users.noreply.github.com>
2023-07-05 15:01:08 +0500

    связи поправлены + геттеры, сеттеры

--- src/main/java/com/actiongroup/actionserver/models/stats/Review.java
+++ src/main/java/com/actiongroup/actionserver/models/stats/Review.java
@@ -11,18 +11,55 @@ public class Review{
     @GeneratedValue
     private long id;
 
-    @OneToOne()
+    @ManyToOne()
     @JoinColumn(name="author_id", referencedColumnName = "id")
     private User author;
 
 
-    @OneToOne()
+    @ManyToOne()
     @JoinColumn(name="event_id", referencedColumnName = "id")
     private Event event;
 
     private int rating;
 
     private String content;
-    
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
 
