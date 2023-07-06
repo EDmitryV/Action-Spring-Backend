29186f09f69ebe0eabd608e2a1fc0b5222ac0eae
EDmitryV <74673717+EDmitryV@users.noreply.github.com>
2023-07-06 13:37:43 +0500

    User's service and repository required (#3)

---
+++ src/main/java/com/actiongroup/actionserver/repositories/EventRepository.java
@@ -0,0 +1,15 @@
+package com.actiongroup.actionserver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.actiongroup.actionserver.models.events.Event;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    //TODO add queries for dateTime, author and filters
+    @Query(value = "SELECT * FROM event_table e WHERE ST_Within(e.point_column, ST_MakeEnvelope(:minX, :minY, :maxX, :maxY, 4326))", nativeQuery = true)
+    List<Event> findEventsOnMap(@Param("minX") double minX, @Param("minY") double minY, @Param("maxX") double maxX, @Param("maxY") double maxY);
+}
