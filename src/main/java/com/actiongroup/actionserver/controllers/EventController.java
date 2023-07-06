efb0d976fea6f3f086df1719fede8cd3e0c8e69b
EDmitryV <edv.busines@gmail.com>
2023-07-06 11:56:26 +0500

    User's service and repository required

--- src/main/java/com/actiongroup/actionserver/controllers/EventController.java
+++ src/main/java/com/actiongroup/actionserver/controllers/EventController.java
@@ -1,23 +1,71 @@
 package com.actiongroup.actionserver.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import com.actiongroup.actionserver.models.events.Event;
+import com.actiongroup.actionserver.services.EventService;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// public class EventController{
-// 	@GetMapping("/greeting")
-// 	public List<Event> getEventsOnMap(@RequestParam(value = "filters[]") String[] filters), @RequestParam(value = "start_latitude")  startPoint {
-// 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-// 	}
-// 	}@GetMapping("/greeting")
-// 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-// 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-// 	}
-// }@GetMapping("/greeting")
-// 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		// return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	// }
-// }
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/event")
+public class EventController {
+    private final EventService eventService;
+
+    @Autowired
+    EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    public static boolean isValidFormat(String format, String value) {
+        Date date = null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            date = sdf.parse(value);
+            if (!value.equals(sdf.format(date))) {
+                date = null;
+            }
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+        return date != null;
+    }
+
+//    @GetMapping("/get-on-map")
+//    ResponseEntity<List<Event>> getOnMap(
+//            @RequestParam(value = "startPoint[]") Double[] startPoint,
+//            @RequestParam(value = "endPoint[]") Double[] endPoint,
+//            @RequestParam(value = "date_and_time") String dateAndTimeFilter,
+//            @RequestParam(value = "from_me") Boolean fromMe,
+//            @RequestParam(value = "from_subscriptions") Boolean fromSubscriptions,
+//            @RequestParam(value = "from_friends") Boolean fromFriends,
+//            @RequestParam(value = "filter_categories[]") String[] filterCategories
+//    ) {
+//        GeometryFactory geometryFactory = new GeometryFactory();
+//        List<String> possibleDateTimeFilters = Arrays.asList("now", "today", "week", "month");
+//        if (startPoint.length != 2 || endPoint.length != 2 || (!possibleDateTimeFilters.contains(dateAndTimeFilter) && !isValidFormat("dd-MM-yyyy", dateAndTimeFilter))) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//        return new ResponseEntity<List<Event>>(eventService.getEventsOnMap(
+//                geometryFactory.createPoint(new Coordinate(startPoint[0], startPoint[1])),
+//                geometryFactory.createPoint(new Coordinate(endPoint[0], endPoint[1])),
+//                dateAndTimeFilter,
+//                fromMe,
+//                fromSubscriptions,
+//                fromFriends,
+//                filterCategories
+//                ), HttpStatus.OK);
+//    }
+}
---
+++ src/main/java/com/actiongroup/actionserver/services/EventService.java
@@ -0,0 +1,39 @@
+package com.actiongroup.actionserver.services;
+
+import com.actiongroup.actionserver.models.events.Event;
+import com.actiongroup.actionserver.models.users.User;
+import com.actiongroup.actionserver.repositories.EventRepository;
+import org.locationtech.jts.geom.Point;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EventService {
+    private final EventRepository eventRepository;
+    @Autowired
+    public EventService(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getEventsOnMap(Point startPosition, Point endPosition, String dateTimeFilter, Boolean fromMe, Boolean fromSubscriptions, Boolean fromFriends, String[] filterCategories, User user){
+        List<Double> authorsIds = new ArrayList<>();
+        if (fromMe){
+            //TODO add user_id to authorsIds
+        }
+        if (fromSubscriptions){
+            //TODO add user_subscriptions to authorIds
+        }
+        if (fromFriends){
+            //TODO add user_friends to authorIds
+        }
+        List<LocalDateTime> startDateTimes = new ArrayList<>();
+        //TODO move available filters here?
+//        if(dateTimeFilter == )
+        //TODO check for min and max Y
+        return eventRepository.findEventsOnMap(startPosition.getX(), startPosition.getY(), endPosition.getX(), endPosition.getY());
+    }
+}
