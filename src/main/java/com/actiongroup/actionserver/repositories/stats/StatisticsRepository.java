86b637f226b38fb75ccc4af39100e3ec26035460
stlad <82213506+stlad@users.noreply.github.com>
2023-07-08 08:43:00 +0500

    создание + удаление ивента и его статистики

--- src/main/java/com/actiongroup/actionserver/repositories/stats/StatisticsRepository.java
+++ src/main/java/com/actiongroup/actionserver/repositories/stats/StatisticsRepository.java
@@ -1,9 +1,10 @@
 package com.actiongroup.actionserver.repositories.stats;
 
+import com.actiongroup.actionserver.models.events.Event;
 import com.actiongroup.actionserver.models.stats.Statistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatisticsRepository extends JpaRepository<Statistics,Long> {
-
+    public Statistics findByEvent(Event event);
 
 }
