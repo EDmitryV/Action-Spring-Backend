37f668e8f21c1eb10ef236c1c07cdb2c5f943a12
edvso <edv.busines@gmail.com>
2023-07-13 23:15:43 +0500

    Authentication bug fixed

--- src/main/java/com/actiongroup/actionserver/repositories/users/UserRepository.java
+++ src/main/java/com/actiongroup/actionserver/repositories/users/UserRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String username);
-    User findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
 
     Boolean existsByEmail(String email);
     Boolean existsByUsername(String username);
